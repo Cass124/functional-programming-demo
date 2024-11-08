1c40256269e4008a7f30426d7acf5c87718dc49a
Cass124 <102390731+Cass124@users.noreply.github.com>
2024-11-07 20:01:33 -0500

    FInished Tutorial

--- src/main.hs
+++ src/main.hs
@@ -1,11 +1,42 @@
 -- Write printAMessage here
 
+printAMessage :: Show a => a -> IO ()
+printAMessage x = print x
+
 -- Write division here
+
+
+division :: Double -> Double -> Maybe Double
+division _ 0 = Nothing
+division x y = Just(x/y)  
 
 -- Write factorial here
 
+factorial :: Int -> Int
+factorial 0 = 1 
+factorial 1 = 1 
+factorial n = n * factorial (n - 1) 
+
 -- Write factList here
 
--- Write merge here
+factList :: Int -> [Int]
+factList n = [factorial i | i <- [1..n]]
 
-main = print "Test" -- Replace this with your testing code
+-- Write merge here
+merge :: [Int] -> [Int] -> [Int]
+merge [] ls2 = ls2
+merge ls1 [] = ls1
+merge (x:xs) (y:ys)
+    | x <= y    = x : merge xs (y:ys)
+    | otherwise = y : merge (x:xs) ys  
+
+main :: IO ()
+main = do
+    printAMessage("Test printAMessage")
+    print (division 10 2) 
+    print (division 10 0)  
+    print (division 5 2)  
+    print (factorial 5)      
+    print (factList 5)
+    print (merge [1, 3, 5] [2, 4, 6])
+    print (merge [-1,-2,-3] [1,2,3])
--- src/test.hs
+++ src/test.hs
@@ -1,2 +1,29 @@
 -- Feel free to experiment with Haskell in this file!
 -- This will not affect your assignment's grade
+addNums :: Int -> Int
+addNums 0 = 0
+addNums n = n + addNums (n - 1)
+
+
+
+
+-- Adds together the values taken to the given power of each number in the list.
+sumPowers :: Int -> [Int] -> Int
+sumPowers _ [] = 0
+sumPowers power (x : xs) = (x ^ power) + sumPowers power xs
+
+sumPowers2 = sumPowers 2
+sumPowers3 = sumPowers 3
+
+sum1 = sumPowers2 [1, 2, 3, 4] -- 30
+sum2 = sumPowers3 [1, 2, 3, 4] -- 100
+
+
+
+main :: IO ()
+main = do
+    print (addNums 10)
+    print (sum1)
+    print (sum2)
+    print (sumPowers2 [1, 2, 3, 4])
+    print (sumPowers3 [1,2,3,4])
